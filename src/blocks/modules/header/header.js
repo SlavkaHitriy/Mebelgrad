4b87d798f46ca0c8e7d3e809e453f7873fc980ef
Slavka <slavik.nukutuk@gmail.com>
2020-02-10 21:57:10 +0200

    mobile footer has been done to 500px and js has been fixed

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,4 +1,4 @@
-document.querySelector('.burger').onclick = function(e){
+document.querySelector('.burger').addEventListener("click", function(e){
    e.target.parentNode.classList.toggle('active');
    document.querySelector('.aside').classList.toggle('show');
-}
+})
