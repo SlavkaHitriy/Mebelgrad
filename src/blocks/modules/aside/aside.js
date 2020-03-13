ec9f318f8c6918682f45eca2bbf5c6effb0bf8ee
Slavka <slavik.nukutuk@gmail.com>
2020-03-13 23:40:44 +0200

    fixed designer's fixes

--- src/blocks/modules/aside/aside.js
+++ src/blocks/modules/aside/aside.js
@@ -1,16 +1,24 @@
-var offsetTop = document.querySelector('.aside').getBoundingClientRect().top + pageYOffset;
+// var offsetTop = document.querySelector('.aside').getBoundingClientRect().top + pageYOffset;
+var offsetTop = document.querySelector('.header__bottom').getBoundingClientRect().top + pageYOffset + 3;
 
-window.addEventListener('scroll', function() {
-   if(window.innerWidth > 750){
-      if(pageYOffset > offsetTop){
+window.addEventListener('scroll', function () {
+   if (window.innerWidth > 750) {
+      if (pageYOffset > offsetTop) {
          document.querySelector('.aside').classList.add('fixed');
+         document.querySelector('.header__bottom').classList.add('fixed');
+         document.querySelector('.header__logo').classList.add('fixed');
       } else {
          document.querySelector('.aside').classList.remove('fixed');
+         document.querySelector('.header__bottom').classList.remove('fixed');
+         document.querySelector('.header__logo').classList.remove('fixed');
       }
    } else {
+      document.querySelector('.aside').classList.remove('fixed');
+      document.querySelector('.header__bottom').classList.remove('fixed');
+      document.querySelector('.header__logo').classList.remove('fixed');
       return false;
    }
-   
- });
+
+});
 
 
--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,6 +1,25 @@
 let burger = document.querySelector('.burger');
 
-burger.addEventListener("click", function(e){
+burger.addEventListener("click", function () {
    burger.classList.toggle('active');
    document.querySelector('.aside').classList.toggle('show');
 })
+
+// function windowOnResize() {
+//    if (window.innerWidth < 750) {
+
+//       })
+
+
+
+//    } else {
+
+//    }
+// }
+
+// window.addEventListener('resize', function () {
+//    windowOnResize();
+// })
+
+// windowOnResize();
+
